f97ce33e119f81527012be3f83fb4b7942f165be
Yazid Bintang <yazidbintang2711@gmail.com>
2025-02-02 16:10:38 +0700

    Create light and dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_app/data/service/api_service.dart';
+import 'package:restaurant_app/presentation/providers/home/restaurant_list_provider.dart';
+import 'package:restaurant_app/presentation/providers/main/index_nav_provider.dart';
+import 'package:restaurant_app/presentation/providers/search/restaurant_search_provider.dart';
+import 'package:restaurant_app/presentation/providers/theme/theme_provider.dart';
+import 'package:restaurant_app/presentation/routes/app_router.dart';
+import 'package:restaurant_app/presentation/styles/theme/app_theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => IndexNavProvider()),
+      Provider(create: (context) => ApiService()),
+      ChangeNotifierProvider(
+          create: (context) =>
+              RestaurantListProvider(context.read<ApiService>())),
+      ChangeNotifierProvider(
+          create: (context) =>
+              RestaurantSearchProvider(context.read<ApiService>())),
+      ChangeNotifierProvider(create: (context) => ThemeProvider())
+    ],
+    child: const MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +30,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: Provider.of<ThemeProvider>(context).isDark ? ThemeMode.light : ThemeMode.dark,
+      routerConfig: AppRouter.router,
     );
   }
 }
