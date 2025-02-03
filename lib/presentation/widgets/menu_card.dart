222d6ce6474c32d480c4e21a8c7d8ff43ff67714
Yazid Bintang <yazidbintang2711@gmail.com>
2025-02-03 23:38:21 +0700

    Create menu widget

---
+++ lib/presentation/widgets/menu_card.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MenuCard extends StatelessWidget {
+  final String menu;
+  const MenuCard({super.key, required this.menu});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+          border: Border.all(
+              width: 1, color: Theme.of(context).colorScheme.tertiary)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        child: Center(
+          child: Text(
+            menu,
+            style: Theme.of(context).textTheme.bodyLarge,
+            textAlign: TextAlign.center,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+      ),
+    );
+  }
+}
