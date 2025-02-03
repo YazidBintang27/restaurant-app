b1086e698c230ae1fba33102fcc5ec8bb81ae7d7
Yazid Bintang <yazidbintang2711@gmail.com>
2025-02-03 23:38:36 +0700

    Create review widget

---
+++ lib/presentation/widgets/review_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ReviewCard extends StatelessWidget {
+  final String name;
+  final String review;
+  final String date;
+
+  const ReviewCard({super.key, required this.name, required this.review, required this.date});
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(minHeight: 60, maxHeight: 80, minWidth: 180, maxWidth: 180),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                '$name - ',
+                style: Theme.of(context).textTheme.titleSmall,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                date,
+                style: Theme.of(context)
+                    .textTheme
+                    .titleSmall
+                    ?.copyWith(color: Theme.of(context).colorScheme.tertiary),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 2,
+          ),
+          Text(
+            review,
+            style: Theme.of(context).textTheme.titleSmall,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ],
+      ),
+    );
+  }
+}
